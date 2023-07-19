be5a4f1d85c14b63184e30dae7886275619d0d82
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-07-19 17:08:54 +0600

    done for today

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/register", (_, res) => res.send("yes"));
+router.post("/register", (req, res) => {
+    
+});
 
 module.exports = router;
