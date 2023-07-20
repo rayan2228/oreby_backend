b7a1d1f41a1d0f672717edd8fb035b64d3e41c2a
rayan <rhrayan2228@gmail.com>
2023-07-20 22:43:56 +0600

    add some error message

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -4,25 +4,25 @@ const passwordValidation = require("../../../helpers/passwordValidation");
 const router = express.Router();
 
 router.post("/register", (req, res) => {
-  const errors = {};
+  const errors = {iserror:true,errorType:"validation error",errors:{}};
   let flag = false;
   let { fullName, email, password } = req.body;
   if (!fullName) {
-    errors.fullName = "name is required";
+    errors.errors.fullName = "name is required";
     flag = true;
   }
   if (!email) {
-    errors.email = "email is required";
+    errors.errors.email = "email is required";
     flag = true;
   } else if (!emailValidation(email)) {
-    errors.email = "invalid email";
+    errors.errors.email = "invalid email";
      flag = true;
   }
   if (!password) {
-    errors.password = "password is required";
+    errors.errors.password = "password is required";
      flag = true;
   } else if (!passwordValidation(password)) {
-    errors.password = "give a strong password";
+    errors.errors.password = "give a strong password";
      flag = true;
   }
   if (flag) {
