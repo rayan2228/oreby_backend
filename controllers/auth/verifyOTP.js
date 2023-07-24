63b1886005430a9ed78596773a84d27892ba3f11
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-07-24 17:58:26 +0600

    email verify with otp done

---
+++ controllers/auth/verifyOTP.js
@@ -0,0 +1,36 @@
+const userModal = require("../../modal/userModal");
+
+const verifyOTP = async (req, res) => {
+  const errors = {
+    iserror: true,
+    errorType: "email verification error",
+    errors: {},
+  };
+  let { email, otp } = req.body;
+  if (email && otp) {
+    let matchOTP = await userModal.find({ email });
+    if (matchOTP[0].emailOTP === otp) {
+      await userModal.findOneAndUpdate(
+        { email },
+        { $set: { emailOTP: "", emailVerified: true } },
+        { new: true }
+      );
+      res.json({
+        iserror: false,
+        message: "email verified successfully",
+        data: {
+          fullName: matchOTP[0].fullName,
+          email: email,
+        },
+      });
+    } else {
+      errors.errors.verification = "otp not matched,try again";
+      res.send(errors);
+    }
+  } else {
+    errors.errors.verification = "otp not matched,try again";
+    res.send(errors);
+  }
+};
+
+module.exports = verifyOTP;
--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const registerController = require("../../../controllers/auth/registerController");
 const loginController = require("../../../controllers/auth/loginController");
+const verifyOTP = require("../../../controllers/auth/verifyOTP");
 const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
+router.post("/verifyemail", verifyOTP);
 
 module.exports = router;
