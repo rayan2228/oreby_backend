efc8bc20500b5d0586ca0c2b2571eaa988e0e1b9
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-07-31 12:51:08 +0600

    category duplicate validation

--- controllers/category/categoryController.js
+++ controllers/category/categoryController.js
@@ -16,20 +16,26 @@ const categoryController = async (req, res) => {
     errors.errors.name = "category name is required";
     res.send(errors);
   } else {
-    if (!slug) {
-      let genarateSlug = name.toLowerCase().replaceAll(" ", "-");
-      slug = genarateSlug;
+    let duplicateCategory = await categoryModal.find({ name });
+    if (duplicateCategory.length > 0) {
+      errors.errors.name = "category name is already used";
+      res.send(errors);
     } else {
-      const categoryData = new categoryModal({
-        name,
-        description,
-        slug,
-      });
-      await categoryData.save();
-      data.data.name = name;
-      data.data.slug = slug;
-      data.data.description = description;
-      res.send(data);
+      if (!slug) {
+        let genarateSlug = name.toLowerCase().replaceAll(" ", "-");
+        slug = genarateSlug;
+      } else {
+        const categoryData = new categoryModal({
+          name,
+          description,
+          slug,
+        });
+        await categoryData.save();
+        data.data.name = name;
+        data.data.slug = slug;
+        data.data.description = description;
+        res.send(data);
+      }
     }
   }
 };
